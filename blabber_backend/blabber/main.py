0459b4243cb66916b2d33e661427e55082977fa2
sparsh-Tyagi01 <sparshtyagi630@gmail.com>
2025-06-08 13:21:11 +0530

    change CORS header in fastapi

--- blabber_backend/blabber/main.py
+++ blabber_backend/blabber/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://blabber-chi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
